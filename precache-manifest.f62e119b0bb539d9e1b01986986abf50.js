5ae7841289234432b24fd88c327314d9664a8166
Sidda Chalamalasetty <tosidda@gmail.com>
2020-03-11 15:38:10 -0400

    Updates

---
+++ precache-manifest.f62e119b0bb539d9e1b01986986abf50.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17fcd08cc9843418ffe060449b65e23b",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "fa5eaa67f782d6a9b38a",
+    "url": "/porfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb95d75eff9dbb0dd346",
+    "url": "/porfolio/static/js/2.add3a0f6.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/porfolio/static/js/2.add3a0f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa5eaa67f782d6a9b38a",
+    "url": "/porfolio/static/js/main.fc6939b7.chunk.js"
+  },
+  {
+    "revision": "99dd6d535eee4ec0fd7e",
+    "url": "/porfolio/static/js/runtime-main.6602b64a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/porfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e0186d29fabc77dcd548be77dd7742e1",
+    "url": "/porfolio/static/media/sidda.e0186d29.jpg"
+  }
+]);
